d6f3f8d50d65f48e0f3d01459aa2c39e882fdcd5
fonhal <fonhal@hotmail.com>
2019-01-12 15:24:15 +0800

    修复构建错误

--- client/components/Footer/Footer.js
+++ client/components/Footer/Footer.js
@@ -1,64 +1,59 @@
-import './Footer.scss';
-import React, { PureComponent as Component } from 'react';
-import PropTypes from 'prop-types';
-import { Row, Col } from 'antd';
-import { Icon } from 'antd';
+import './Footer.scss'
+import React, { PureComponent as Component } from 'react'
+import PropTypes from 'prop-types'
+import { Row, Col } from 'antd'
+import { Icon } from 'antd'
 
 /* const version = process.env.version; */
 class Footer extends Component {
   constructor(props) {
-    super(props);
+    super(props)
   }
   static propTypes = {
     footList: PropTypes.array
-  };
+  }
   render() {
     return (
       <div className="footer-wrapper">
         <Row className="footer-container">
+          {' '}
           {this.props.footList.map(function(item, i) {
-            return (
-              <FootItem
-                key={i}
-                linkList={item.linkList}
-                title={item.title}
-                iconType={item.iconType}
-              />
-            );
-          })}
-        </Row>
+            return <FootItem key={i} linkList={item.linkList} title={item.title} iconType={item.iconType} />
+          })}{' '}
+        </Row>{' '}
       </div>
-    );
+    )
   }
 }
 
 class FootItem extends Component {
   constructor(props) {
-    super(props);
+    super(props)
   }
   static propTypes = {
     linkList: PropTypes.array,
     title: PropTypes.string,
     iconType: PropTypes.string
-  };
+  }
   render() {
     return (
       <Col span={24}>
         <h4 className="title">
-          {this.props.iconType ? <Icon type={this.props.iconType} className="icon" /> : ''}
-          {this.props.title}
-        </h4>
+          {' '}
+          {this.props.iconType ? <Icon type={this.props.iconType} className="icon" /> : ''} {this.props.title}{' '}
+        </h4>{' '}
         {this.props.linkList.map(function(item, i) {
           return (
             <p key={i}>
               <a href={item.itemLink} className="link">
-                {item.itemTitle}
-              </a>
+                {' '}
+                {item.itemTitle}{' '}
+              </a>{' '}
             </p>
-          );
-        })}
+          )
+        })}{' '}
       </Col>
-    );
+    )
   }
 }
 
@@ -99,7 +94,7 @@ Footer.defaultProps = {
       ]
     },*/
     {
-      title: 'Copyright © 2018 CMS Web Group',
+      title: 'Copyright © 2018 CMS Web Group'
       /* linkList: [
         {
           itemTitle: `版本: ${version} `,
@@ -112,6 +107,6 @@ Footer.defaultProps = {
       ] */
     }
   ]
-};
+}
 
-export default Footer;
+export default Footer
