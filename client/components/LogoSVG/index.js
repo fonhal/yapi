4b3c9ee3710e853839a936f1934edc3832a834ab
fonhal <fonhal@hotmail.com>
2019-01-20 14:40:56 +0800

    修改了构建错误

--- client/components/LogoSVG/index.js
+++ client/components/LogoSVG/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const LogoSVG = props => {
   return (
     <img src="/image/logo.png" />
   );
-};
-
-LogoSVG.propTypes = {
-  length: PropTypes.any
 };
 
 export default LogoSVG;
